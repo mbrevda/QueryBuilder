da1411def961e09e8b14d347ec8b14d9be4ba4a2
Moshe Brevda <mbrevda@gmail.com>
2015-07-28 16:23:48 +0300

    There should always be to specs now

--- src/Connectives/AndX.php
+++ src/Connectives/AndX.php
@@ -9,12 +9,10 @@ class AndX extends CompositeQuery
     protected $specification1;
     protected $specification2;
 
-    public function __construct($specification1, $specification2 = '')
+    public function __construct($specification1, $specification2)
     {
         $this->specification1 = $specification1->selectSatisfying($this);
-        $this->specification2 = $specification2
-            ? $specification2->selectSatisfying($this)
-            : null;
+        $this->specification2 = $specification2->selectSatisfying($this);
 
         return $this;
     }
